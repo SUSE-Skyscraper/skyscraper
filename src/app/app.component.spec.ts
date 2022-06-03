0f2cdf6d2ddc95eec0c5b45d6b94ecd688bfcd5e
Thomas Muntaner <thomas.muntaner@gmail.com>
2022-06-03 12:44:20 +0200

    Added github actions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('skyscraper-web');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'skyscraper-web app is running!',
-    );
-  });
 });
