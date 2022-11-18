3568ee7e7e7ab157672c2441ccd71d8e81057685
Thomas Muntaner <thomas.muntaner@gmail.com>
2022-11-18 09:45:05 +0100

    Update to angular 15

--- web/src/test.ts
+++ web/src/test.ts
@@ -15,24 +15,8 @@ ngMocks.defaultMock(OktaAuthStateService, () => ({
   authState$: EMPTY,
 }));
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp,
-  ): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
