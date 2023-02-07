5c76a76e6f31d5d7f61ce8fcddf27788a045f58f
Thomas Muntaner <thomas.muntaner@gmail.com>
2023-02-07 09:51:48 +0100

    Added payload tests for api keys and cloud accounts
    Note: Update cloud account payload was no longer used, so it was
    removed.

---
+++ api/payloads/cloud_accounts_test.go
@@ -0,0 +1,50 @@
+package payloads
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssignCloudAccountToOUPayload_Bind(t *testing.T) {
+	tc := []struct {
+		name        string
+		ouID        string
+		expectError bool
+	}{
+		{
+			name:        "valid ou id",
+			ouID:        uuid.New().String(),
+			expectError: false,
+		},
+		{
+			name:        "invalid ou id",
+			ouID:        "foobar",
+			expectError: true,
+		},
+		{
+			name:        "empty ou id",
+			ouID:        "",
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			payload := AssignCloudAccountToOUPayload{
+				Data: AssignCloudAccountToOUPayloadData{
+					OrganizationalUnitID: tt.ouID,
+				}}
+			err := payload.Bind(nil)
+			if tt.expectError {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
+			if err == nil && tt.ouID != "" {
+				assert.Equal(t, tt.ouID, payload.Data.GetOrganizationalUnitID().String())
+			}
+		})
+	}
+}
